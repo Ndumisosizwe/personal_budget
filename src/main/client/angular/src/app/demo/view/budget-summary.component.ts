005e9c32cc70feede46668264c7401e526fceb77
ndumiso <ndumisosizwe@gmail.com>
2018-08-20 05:48:35 +0200

    Budget Summary Screen, added bread crumb

--- src/main/client/angular/src/app/demo/view/budget-summary.component.ts
+++ src/main/client/angular/src/app/demo/view/budget-summary.component.ts
@@ -1,19 +1,36 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {BudgetService} from "../service/budget-service";
 import {Budget} from "../budget.ts/budget";
+import {MenuItem} from "primeng/api";
 
 @Component({
     templateUrl: './budget-summary.component.html'
 })
-export class BudgetSummaryComponent {
+export class BudgetSummaryComponent implements OnInit {
 
     protected budgets: Budget[] = [];
+    breadcrumbItems: MenuItem[];
+    homeIcon: MenuItem;
 
     constructor(private budgetService: BudgetService) {
         budgetService.getAllBudgets().subscribe((budgets: Budget[]) => {
             this.budgets = budgets;
             console.log(this.budgets);
         });
+    }
+
+    ngOnInit() {
+        this.homeIcon = {icon: 'pi pi-home'};
+        this.breadcrumbItems = [
+            {label: 'Categories'},
+            {label: 'Sports'},
+            {label: 'Football'},
+            {label: 'Countries'},
+            {label: 'Spain'},
+            {label: 'F.C. Barcelona'},
+            {label: 'Squad'},
+            {label: 'Lionel Messi', url: 'https://en.wikipedia.org/wiki/Lionel_Messi'}
+        ];
     }
 
 }
