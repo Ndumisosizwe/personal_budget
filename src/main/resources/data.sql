fef43045f6461f672a7ee51a0be03402fb4f8a7f
Ndumiso <ndumiso.mhlongo@supergrp.com>
2018-09-26 19:13:53 +0200

    updated data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,7 +10,7 @@ INSERT INTO budget_period VALUES
 INSERT INTO budget VALUES (1, 'NBS MHLONGO', now(), 10040, 0.0, 1);
 INSERT INTO budget VALUES (2, 'NBS MHLONGO', now(), 9441, 0.0, 2);
 INSERT INTO budget VALUES (3, 'NBS MHLONGO', now(), 10040, 0.0, 3);
-INSERT INTO budget VALUES (4, 'NBS MHLONGO', now(), 9059, 0.0, 4);
+INSERT INTO budget VALUES (4, 'NBS MHLONGO', now(), 7387, 0.0, 4);
 
 -- transactions in JUNE 2018
 INSERT INTO transaction VALUES
@@ -37,15 +37,17 @@ INSERT INTO transaction VALUES
 
 -- transactions in SEPTEMBER 2018
 INSERT INTO transaction VALUES
-  (15, 'NBS MHLONGO', now(), 21819, 'Monthly Salary (Nett)', 'INCOME', 4),
+  (15, 'NBS MHLONGO', now(), 24297, 'Monthly Salary (Nett)', 'INCOME', 4),
   (16, 'NBS MHLONGO', now(), 2700, 'Car Installment MFC', 'EXPENSE', 4),
   (17, 'NBS MHLONGO', now(), 1060, 'Car Insurance Mi Way', 'EXPENSE', 4),
-  (18, 'NBS MHLONGO', now(), 3500, 'Monthly Rental', 'EXPENSE', 4),
+  (18, 'NBS MHLONGO', now(), 4000, 'Monthly Rental', 'EXPENSE', 4),
   (19, 'NBS MHLONGO', now(), 1000, 'Discovery Medical Aid', 'EXPENSE', 4),
   (20, 'NBS MHLONGO', now(), 1200, 'Telkom Mobile & LTE', 'EXPENSE', 4),
   (21, 'NBS MHLONGO', now(), 1500, 'FNB Credit Card Repayment', 'EXPENSE', 4),
   (22, 'NBS MHLONGO', now(), 300, 'NSFAS Repayment', 'EXPENSE', 4),
-  (23, 'NBS MHLONGO', now(), 1500, 'Fuel for new month (October)', 'EXPENSE', 4);
+  (23, 'NBS MHLONGO', now(), 650, 'Fuel for new month (October)', 'EXPENSE', 4),
+  (24, 'NBS MHLONGO', now(), 2500, 'Allan Gray Retirement Annuity', 'EXPENSE', 4);
+  (24, 'NBS MHLONGO', now(), 2000, 'Savings Flexi', 'EXPENSE', 4);
 
 
 
